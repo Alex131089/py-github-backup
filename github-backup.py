8b7fc11869457e94cb7f2d1f7b8a8305569412dc
Samuel Walladge <samuel@swalladge.id.au>
2016-06-29 21:47:08 +0930

    massive refactor - old code was awful

--- github-backup.py
+++ github-backup.py
@@ -1,9 +1,7 @@
 import requests
 import subprocess
 import os
-import sys
 import argparse
-import re
 
 parser = argparse.ArgumentParser(description='Backup (clone) github repos.')
 
@@ -28,34 +26,30 @@ args = parser.parse_args()
 user = args.username
 auth = (user, args.token) if args.token else None
 
-repos = {
-        'repos': {},
-        'starred-repos': {},
-        'gists': {},
-        'starred-gists': {}
-        }
+repos = []
 
 
-# function to get a list of data about repos, given api url, and optional params
-# (assuming response is github's standard list of repositories in json format)
-def get_repos(data):
-    url = data.get('url')
-    params = data.get('params', None)
-    key = data.get('clone-key', 'clone_url')
-    format_url = data.get('format', None)
-    new_repos = []
+class Repo():
+    def __init__(self):
+        self.url = None
+        self.name = None
+        self.owner = None
+        self.subdir = None
+
+
+# function to get json data from server
+def get_json(url, params=None, auth=None):
+    data = []
     while True:
         res = None
-        res_repos = None
+        js = None
         try:
             res = requests.get(url, params=params, auth=auth)
-            res_repos = res.json()
+            js = res.json()
         except:
-            print("error getting repo information for url {}".format(url))
-            sys.exit(1)
-        for repo in res_repos:
-            new_repos.append({'owner': repo['owner']['login'],
-                          'clone_url': format_url.format(repo[key]) if format_url else repo[key]})
+            print("error getting data for url {}".format(url))
+        for j in js:
+            data.append(j)
 
         next_link = res.links.get('next', None)
         if next_link:
@@ -63,101 +57,108 @@ def get_repos(data):
         else:
             break
 
-    return new_repos
+    return data
 
-urls = []
 
 # user repos
 if args.all or args.repos:
+    params = {'type': 'all'}
+
     if args.token:
-        urls.append({
-            'url': 'https://api.github.com/user/repos',
-            'params': {'type': 'all'},
-            'type': 'repos',
-            'clone-key': 'ssh_url' if args.ssh else 'clone_url'
-            })
+        data = get_json('https://api.github.com/user/repos', params=params, auth=auth)
     else:
         print('no auth - private repos not going to be fetched')
-        urls.append({
-            'url': 'https://api.github.com/users/{}/repos'.format(user),
-            'params': {'type': 'all'},
-            'type': 'repos',
-            'clone-key': 'ssh_url' if args.ssh else 'clone_url'
-            })
+        data = get_json('https://api.github.com/users/{}/repos'.format(user), params=params, auth=auth)
+
+    for repo in data:
+        r = Repo()
+        r.owner = repo['owner']['login']
+        r.name = repo['name']
+        if args.ssh:
+            r.url = repo['ssh_url']
+        else:
+            r.url = repo['clone_url']
+
+        r.subdir = 'repos'
+        repos.append(r)
 
 # user starred repos
 if args.all or args.starred:
-    urls.append({
-        'url': 'https://api.github.com/users/{}/starred'.format(user),
-        'type': 'starred-repos',
-        'clone-key': 'ssh_url' if args.ssh else 'clone_url'
-        })
+    data = get_json('https://api.github.com/users/{}/starred'.format(user), auth=auth)
+    for repo in data:
+        r = Repo()
+        r.owner = repo['owner']['login']
+        r.name = repo['name']
+        if args.ssh:
+            r.url = repo['ssh_url']
+        else:
+            r.url = repo['clone_url']
+
+        r.subdir = 'starred-repos'
+        repos.append(r)
 
 # user gists
 if args.all or args.gists:
-    urls.append({
-        'url': 'https://api.github.com/users/{}/gists'.format(user),
-        'type': 'gists',
-        'clone-key': 'git_pull_url' if not args.ssh else 'id',
-        'format': 'git@gist.github.com:{}.git' if args.ssh else None
-        })
+    data = get_json('https://api.github.com/users/{}/gists'.format(user), auth=auth)
+    for repo in data:
+        r = Repo()
+        r.owner = repo['owner']['login']
+        r.name = repo['id']
+        if args.ssh:
+            r.url = 'git@gist.github.com:{}.git'.format(repo['id'])
+        else:
+            r.url = repo['git_pull_url']
+
+        r.subdir = 'gists'
+        repos.append(r)
 
 # starred gists
 if args.all or args.starredgists:
     if args.token:
-        urls.append({
-            'url': 'https://api.github.com/gists/starred',
-            'type': 'starred-gists',
-            'clone-key': 'git_pull_url' if not args.ssh else 'id',
-            'format': 'git@gist.github.com:{}.git' if args.ssh else None
-            })
+        data = get_json('https://api.github.com/gists/starred', auth=auth)
+        for repo in data:
+            r = Repo()
+            r.owner = repo['owner']['login']
+            r.name = repo['id']
+            if args.ssh:
+                r.url = 'git@gist.github.com:{}.git'.format(repo['id'])
+            else:
+                r.url = repo['git_pull_url']
+            r.subdir = 'starred-gists'
+
+            repos.append(r)
+
     else:
         print('skipping starred gists - auth required')
-
-
-# build a list of repos to clone/update
-for url in urls:
-    r = get_repos(url)
-    for repo in r:
-        subdir = url.get('type')
-        if repo['owner'] not in repos[subdir]:
-            repos[subdir][repo['owner']] = []
-
-        repos[subdir][repo['owner']].append(repo['clone_url'])
 
 
 # go through the filesystem to build up list of commands to run (whether git
 # clone or update existing) - also make any necessary dirs
 root = os.path.realpath(args.directory)
 to_run = []
-NAME = re.compile(r'^.*(?:/|:)([^/:]+)\.git$')
+for repo in repos:
+    sub_root = os.path.join(root, repo.subdir)
+    os.makedirs(os.path.join(sub_root, repo.owner), exist_ok=True)
+    owner_root = os.path.join(sub_root, repo.owner)
+    path = os.path.join(owner_root, repo.name)
 
-for subdir in repos:
-    sub_root = os.path.join(root, subdir)
-    for owner in repos[subdir]:
-        os.makedirs(os.path.join(sub_root, owner), exist_ok=True)
-        owner_root = os.path.join(sub_root, owner)
-        for url in repos[subdir][owner]:
-            name = NAME.match(url).group(1)
-            path = os.path.join(owner_root, name)
-
-            # check if exists
-            if os.access(path, os.F_OK):
-                # fetch, so shouldn't overwrite any local changes
-                to_run.append({
-                    'path': path,
-                    'command': ['git', 'fetch', '--recurse-submodules=yes', '-t']})
-            else:
-                # this method doesn't clone a bare repo - rather the client-side
-                # style we can work withh straight away
-                to_run.append({
-                    'path': owner_root,
-                    'command': ['git', 'clone', '--recursive', url]})
+    # check if exists
+    if os.access(path, os.F_OK):
+        # fetch, so shouldn't overwrite any local changes
+        to_run.append({
+            'path': path,
+            'command': ['git', 'fetch', '--recurse-submodules=yes', '-t']})
+    else:
+        # this method doesn't clone a bare repo - rather the client-side
+        # style we can work withh straight away
+        to_run.append({
+            'path': owner_root,
+            'command': ['git', 'clone', '--recursive', repo.url]})
 
 
 # TODO: spawn multiple processes at once for parallel downloading
-# actually run the git commands!
 
+# actually run the git commands!
 for command in to_run:
     print(command)
     subprocess.run(command['command'], cwd=command.get('path', None))
